a54e06d78f585668e77ced7013dfc5edbfc832d0
Ponrawoot <158698118+Min-pon@users.noreply.github.com>
2024-02-20 16:38:00 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+
+// Route files
+const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// add body parser
+app.use(express.json());
+
+// add cookie parser
+app.use(cookieParser());
+
+app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port ",
+    PORT
+  )
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
