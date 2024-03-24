bdaaa5fac1e50616d368effd1df3cfdc69dbe38a
Ponrawoot <158698118+Min-pon@users.noreply.github.com>
2024-03-25 01:28:04 +0700

    update VacQ according asssignment 8

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -5,10 +5,20 @@ const {
   createHospital,
   updateHospital,
   deleteHospital,
+  getVacCenters,
 } = require("../controllers/hospitals");
+
+//Include other resource routers
+const appointmentRouter = require("./appointments");
+
 const router = express.Router();
 
 const { protect, authorize } = require("../middleware/auth");
+
+//Re-route into other resource routers
+router.use("/:hospitalId/appointments/", appointmentRouter);
+
+router.route("/vacCenters").get(getVacCenters);
 
 router
   .route("/")
